f77d3f90a97dac78f5e74454e92082f7ac3da144
surya-4409 <suryanarayana9125@gmail.com>
2026-08-08 11:06:26 +0530

    Time: 102 ms (74.03%), Space: 142.3 MB (23.98%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -0,0 +1,31 @@
+class NumMatrix {
+
+    int[][] prefix;
+
+    public NumMatrix(int[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        prefix = new int[rows + 1][cols + 1];
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+
+                prefix[i][j] =
+                    matrix[i - 1][j - 1]
+                    + prefix[i - 1][j]
+                    + prefix[i][j - 1]
+                    - prefix[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+        return prefix[row2 + 1][col2 + 1]
+             - prefix[row1][col2 + 1]
+             - prefix[row2 + 1][col1]
+             + prefix[row1][col1];
+    }
+}
