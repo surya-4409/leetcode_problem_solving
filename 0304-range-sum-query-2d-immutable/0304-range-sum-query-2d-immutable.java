2c0dfcc4c8cf3eb882f7621fe5d0a830a08f54a2
surya-4409 <suryanarayana9125@gmail.com>
2026-08-12 10:52:20 +0530

    Time: 100 ms (85.93%), Space: 142 MB (56.9%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -4,28 +4,30 @@ class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        prefix = new int[rows + 1][cols + 1];
+        prefix = new int[m + 1][n + 1];
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
 
                 prefix[i][j] =
-                    matrix[i - 1][j - 1]
-                    + prefix[i - 1][j]
-                    + prefix[i][j - 1]
-                    - prefix[i - 1][j - 1];
+                        matrix[i - 1][j - 1]
+                        + prefix[i - 1][j]
+                        + prefix[i][j - 1]
+                        - prefix[i - 1][j - 1];
             }
         }
     }
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
+    public int sumRegion(int row1, int col1,
+                         int row2, int col2) {
 
         return prefix[row2 + 1][col2 + 1]
-             - prefix[row1][col2 + 1]
-             - prefix[row2 + 1][col1]
-             + prefix[row1][col1];
+                - prefix[row1][col2 + 1]
+                - prefix[row2 + 1][col1]
+                + prefix[row1][col1];
     }
 }
